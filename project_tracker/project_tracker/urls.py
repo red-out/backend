8ca0d970649c7ed1e8a857d40221710398e09b73
Alexander Filenko <79382290+red-out@users.noreply.github.com>
2024-09-21 21:08:22 +0300

    third commit

--- project_tracker/project_tracker/urls.py
+++ project_tracker/project_tracker/urls.py
@@ -5,13 +5,7 @@ from tasks import views
 urlpatterns = [
     path('', views.cashback_services, name='home'),  # Главная страница с карточками
     path('cashback/<int:id>/', views.GetOrder, name='cashback_url'),  # Детали заказа
-    path('your_cashbacks/', views.categories_cashbacks_view, name='cart'),  # Страница корзины
- 
+    path('your_cashbacks/<int:month_id>/', views.categories_cashbacks_view, name='cart'),  # Страница корзины с month_id
 ]
-
-
-
-
-
 
 
--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -20,7 +20,7 @@ def get_cashbacks():
             'full_description': 'Оплата в заведениях общественного питания.',
             'price': 'Кешбэк за покупки в ресторанах, кафе, барах и сетях быстрого питания, включая доставку еды.',
         },
-        {
+         {
             'id': 3,
             'title': 'Спортивные товары',
             'image_url': 'http://127.0.0.1:9000/web/free-icon-basketball-4645268.png',
@@ -36,64 +36,83 @@ def get_cashbacks():
             'full_description': 'Покупка медикаментов и товаров для здоровья.',
             'price': 'Кешбэк за оплату в аптеках, профильных магазинах товаров для здоровья и онлайн-аптеках.',
         },
-       {
+        {
             'id': 5,
             'title': 'Туризм',
             'image_url': 'http://127.0.0.1:9000/web/free-icon-hiking-1974052.png',
             'details_text': 'Вы получите 7% кешбэка',
             'full_description': 'Оплата туристических услуг, включая бронирование отелей и билетов.',
-            'price': 'Кешбэк за услуги в туристических агентствах, онлайн-сервисах для путешествий и при бронировании экскурсионных туров.'
-},
-{
+            'price': 'Кешбэк за услуги в туристических агентствах, онлайн-сервисах для путешествий и при бронировании экскурсионных туров.',
+        },
+        {
             'id': 6,
             'title': 'Электроника',
             'image_url': 'http://127.0.0.1:9000/web/free-icon-electronics-1692714.png',
             'details_text': 'Вы получите 5% кешбэка',
             'full_description': 'Покупка техники и электроники.',
-            'price': 'Кешбэк за приобретение смартфонов, компьютеров, телевизоров и других гаджетов в магазинах и онлайн-платформах.'
-}
-
+            'price': 'Кешбэк за приобретение смартфонов, компьютеров, телевизоров и других гаджетов в магазинах и онлайн-платформах.',
+        },
+        # Другие категории...
     ]
 
 # Пример данных для корзины
 categories_cashbacks = [
     {
         'order_id': 1,  # Ссылается на идентификатор заказа
+        'month_id': 1,  # Привязка к сентябрю
         'spent': {'amount': 35435, 'range': 'Сентябрь'},
-        'cashback_received': 2480,
         'image_url': 'http://127.0.0.1:9000/web/free-icon-books-4645290.png',
         'details_text': '7% кешбэка по данной категории'
     },
     {
-        'order_id': 2,  # Ссылается на идентификатор заказа
+        'order_id': 2,
+        'month_id': 1,
         'spent': {'amount': 17342, 'range': 'Сентябрь'},
-        'cashback_received': 867,
         'image_url': 'http://127.0.0.1:9000/web/free-icon-restaurant-1689246.png',
         'details_text': '5% кешбэка по данной категории'
     },
     {
-        'order_id': 5,  # Ссылается на идентификатор заказа
-        'spent': {'amount': 9983, 'range': 'Сентябрь'},
-        'cashback_received': 699,
+        'order_id': 3,
+        'month_id': 1,
+        'spent': {'amount': 9779, 'range': 'Сентябрь'},
         'image_url': 'http://127.0.0.1:9000/web/free-icon-hiking-1974052.png',
         'details_text': '7% кешбэка по данной категории'
     },
+    # Другие категории...
 ]
+
+# Отдельный массив для полученного кешбэка
+cashback_received_data = [
+    {'order_id': 1, 'month_id': 1, 'cashback_received': 2480},  # Привязка кешбэка к заказу и месяцу
+    {'order_id': 2, 'month_id': 1, 'cashback_received': 867},
+    {'order_id': 3, 'month_id': 1, 'cashback_received': 685},
+    # Другие данные кешбэков...
+]
+
+# Массив месяцев
+months = {
+    1: 'Сентябрь',
+    # Добавить другие месяцы при необходимости
+}
 
 def cashback_services(request):
     cashbacks = get_cashbacks()
     item_count = len(categories_cashbacks)  # Количество карточек в корзине
 
     # Обработка запроса поиска
-    search_query = request.GET.get('search', '')
+    search_query = request.GET.get('cashback_categories', '')
     if search_query:
         cashbacks = [order for order in cashbacks if search_query.lower() in order['title'].lower()]
+
+    # Указываем id месяца, например, 1 для сентября
+    current_month_id = 1
 
     return render(request, 'index.html', {
         'data': {
             'current_date': date.today(),
             'cashbacks': cashbacks,
-            'cart_item_count': item_count  # Передаем количество карточек в корзине
+            'cart_item_count': item_count,  # Передаем количество карточек в корзине
+            'current_month_id': current_month_id  # Передаем id месяца
         }
     })
 
@@ -107,22 +126,38 @@ def GetOrder(request, id):
 
     return render(request, 'order.html', {'data': item})
 
-def categories_cashbacks_view(request):
+def categories_cashbacks_view(request, month_id):
     cashbacks = get_cashbacks()
     cart_items_with_titles = []
 
-    for item in categories_cashbacks:
+    # Фильтрация данных по месяцу
+    selected_cashbacks = [item for item in categories_cashbacks if item['month_id'] == month_id]
+
+    # Добавляем данные о кешбэке к каждой карточке
+    for item in selected_cashbacks:
         order = next((order for order in cashbacks if order['id'] == item['order_id']), None)
+        cashback_info = next((cashback for cashback in cashback_received_data if cashback['order_id'] == item['order_id'] and cashback['month_id'] == month_id), None)
+
         if order:
             item['title'] = order['title']
+
+        if cashback_info:
+            item['cashback_received'] = cashback_info['cashback_received']
+
         cart_items_with_titles.append(item)
+
+    current_month = months.get(month_id, 'Неизвестный месяц')
 
     return render(request, 'cashbacks.html', {
         'data': {
             'categories_cashbacks': cart_items_with_titles,
-            'current_month': 'Сентябрь'  # или динамически определяемый месяц
+            'current_month': current_month
         }
     })
+
+
+
+
 
 
 
